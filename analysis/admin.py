12776601d34befbd383b7eec3dedcbe24fd8cac5
abhilash01393 <44585670+abhilash01393@users.noreply.github.com>
2020-05-25 03:14:30 -0700

    Add files via upload

---
+++ analysis/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Administator
+# Register your models here.
+
+admin.site.register(Administator)
---
+++ analysis/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class analysisConfig(AppConfig):
+    name = 'analysis'
